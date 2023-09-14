1fbcb99fd3152beb2ef825c1cadc0a54fc9557ec
guswl9814 <guswl9814@fixelsoft.com>
2023-09-14 17:34:23 +0900

    feat: Spring Security 설정 및 테스트 컨트롤러 생성

---
+++ src/main/java/com/responseor/ootw/config/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.responseor.ootw.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/test").permitAll()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/loginProc")
+                .defaultSuccessUrl("/")
+                .and().build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
