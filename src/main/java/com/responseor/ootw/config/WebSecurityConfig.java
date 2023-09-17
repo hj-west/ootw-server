87b6d092d1a4333e2e8544f4a90fdba3c6432850
guswl9814 <guswl9814@fixelsoft.com>
2023-09-18 00:02:58 +0900

    feat: jwt 설정 - WebSecurityConfig.java, JwtTokenProvider.java, JwtAuthenticationFilter.java 추가

--- src/main/java/com/responseor/ootw/config/WebSecurityConfig.java
+++ src/main/java/com/responseor/ootw/config/WebSecurityConfig.java
@@ -1,31 +1,45 @@
 package com.responseor.ootw.config;
 
+import com.responseor.ootw.config.jwt.JwtAuthenticationFilter;
+import com.responseor.ootw.config.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.csrf().disable()
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
                 .authorizeRequests()
                 .antMatchers("/test").permitAll()
                 .anyRequest().permitAll()
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .loginProcessingUrl("/loginProc")
-                .defaultSuccessUrl("/")
-                .and().build();
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+             .build();
     }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
