fd2b9ee0b139fcc04cacc90bd71ec050bb468446
guswl9814 <guswl9814@fixelsoft.com>
2023-09-27 12:43:58 +0900

    feat: ClotheByTemp Model 추가

---
+++ src/main/java/com/responseor/ootw/entity/ClotheByTemp.java
@@ -0,0 +1,46 @@
+package com.responseor.ootw.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "OOTW_CLOTHES_BY_TEMPERATURE")
+public class ClotheByTemp extends BaseEntity{
+    /**
+     * ID : ID
+     */
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * UUID : UUID
+     */
+    @Column(name = "UUID")
+    private Long uuid;
+
+    /**
+     * START_TEMP : 시작 온도
+     */
+    @Column(name = "START_TEMP")
+    private Integer startTemp;
+
+    /**
+     * END_TEMP : 끝 온도
+     */
+    @Column(name = "END_TEMP")
+    private Integer endTemp;
+
+    /**
+     * CLOTHES : 옷 종류
+     */
+    @Column(name = "CLOTHES", length = 1000)
+    private String clothes;
+}
