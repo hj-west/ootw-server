2c4ebb32902fd09705e7c845e945dc83d4f3d04d
201511027-se <guswl9814@gmail.com>
2023-10-02 18:31:32 +0900

    feat: ClothesByTempRepository 생성

---
+++ src/main/java/com/responseor/ootw/repository/ClothesByTempRepository.java
@@ -0,0 +1,17 @@
+package com.responseor.ootw.repository;
+
+import com.responseor.ootw.entity.ClothesByTemp;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ClothesByTempRepository extends JpaRepository<ClothesByTemp, Integer> {
+    @Query(value = "select c from ClothesByTemp c where c.uuid = :uuid and :temp between c.startTemp and c.endTemp")
+    List<ClothesByTemp> findByUuidAndStartTempLessThanEqualOrEndTempGreaterThanEqual(@Param("uuid") Long uuid, @Param("temp") Integer temp);
+
+    @Query(value = "select c from ClothesByTemp c where c.uuid is null and :temp between c.startTemp and c.endTemp")
+    List<ClothesByTemp> findByUuidIsNullAndStartTempLessThanEqualOrEndTempGreaterThanEqual(@Param("temp") Integer temp);
+
+}
