5bfa3d522e62e83fcac935bfadee67d4a0d73f59
201511027-se <guswl9814@gmail.com>
2023-10-02 18:32:30 +0900

    feat: WeatherController.getClothes 구현

--- src/main/java/com/responseor/ootw/controller/WeatherController.java
+++ src/main/java/com/responseor/ootw/controller/WeatherController.java
@@ -1,6 +1,7 @@
 package com.responseor.ootw.controller;
 
 import com.responseor.ootw.dto.WeatherResponseDto;
+import com.responseor.ootw.entity.ClothesByTemp;
 import com.responseor.ootw.service.WeatherService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -19,4 +22,10 @@ public class WeatherController {
     private ResponseEntity<WeatherResponseDto> getWeather(@RequestParam("lat") String lat, @RequestParam("lon") String lon) {
         return ResponseEntity.ok().body(weatherService.getWeather("37.4591444444444", "127.053411111111"));
     }
+
+    @GetMapping("/clothes")
+    private ResponseEntity<List<ClothesByTemp>> getClothes(@RequestParam("temp") Integer temp) {
+        return ResponseEntity.ok().body(weatherService.getClothes(temp));
+    }
+
 }
