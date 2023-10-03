e91a3bc02908f0877a32c8792e9a71d11d8b751a
201511027-se <guswl9814@gmail.com>
2023-10-03 16:48:08 +0900

    feat: WebSecurityConfig Cors 설정 에러 수정(.addAllowedOrigin -> .addAllowedOriginPattern)

--- src/main/java/com/responseor/ootw/config/WebSecurityConfig.java
+++ src/main/java/com/responseor/ootw/config/WebSecurityConfig.java
@@ -54,7 +54,7 @@ public class WebSecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("*");
+        configuration.addAllowedOriginPattern("*");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
