f4cfa301a7d1e0a781066cf2188d26cd616da757
h.j_west <guswl9814@gmail.com>
2023-12-03 23:31:50 +0900

    feat: json 직렬화 이슈를 해결하기 위한 @NoArgsConstructor 어노테이션 추가

--- src/main/java/com/responseor/ootw/dto/member/MemberPasswordUpdateRequestDto.java
+++ src/main/java/com/responseor/ootw/dto/member/MemberPasswordUpdateRequestDto.java
@@ -1,10 +1,14 @@
 package com.responseor.ootw.dto.member;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberPasswordUpdateRequestDto {
     private String newPassword;
     private String confirmPassword;
--- src/main/java/com/responseor/ootw/dto/member/MemberUpdateRequestDto.java
+++ src/main/java/com/responseor/ootw/dto/member/MemberUpdateRequestDto.java
@@ -2,11 +2,13 @@ package com.responseor.ootw.dto.member;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class MemberUpdateRequestDto {
     private String telNo;
 }
