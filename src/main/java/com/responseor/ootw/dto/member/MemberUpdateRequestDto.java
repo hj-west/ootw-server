e20848651406720c9caf48328f51ebc5078d6401
h.j_west <guswl9814@gmail.com>
2023-12-03 22:40:35 +0900

    feat: MemberUpdateRequestDto 생성

---
+++ src/main/java/com/responseor/ootw/dto/member/MemberUpdateRequestDto.java
@@ -0,0 +1,12 @@
+package com.responseor.ootw.dto.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MemberUpdateRequestDto {
+    private String telNo;
+}
