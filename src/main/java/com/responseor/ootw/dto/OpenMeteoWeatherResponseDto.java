fc391adc037a1aff968cea9f3ed151553952733a
guswl9814 <guswl9814@gmail.com>
2023-12-27 17:18:52 +0900

    feat: OpenMeteoWeatherResponseDto 생성

--- src/main/java/com/responseor/ootw/dto/OpenMeteoWeatherResponseDto.java
+++ src/main/java/com/responseor/ootw/dto/OpenMeteoWeatherResponseDto.java
@@ -1,15 +1,13 @@
 package com.responseor.ootw.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class OpenMeteoWeatherResponseDto {
     private Current current;
     private Daily daily;
