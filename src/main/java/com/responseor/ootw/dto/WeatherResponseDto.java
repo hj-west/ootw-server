b2f97b7d6f95bf13103e3d7611456878cecda4c1
guswl9814 <guswl9814@gmail.com>
2023-12-27 17:20:09 +0900

    feat: 불필요 파라미터 삭제 및 api 변경에 따른 스웨거 설명 변경

--- src/main/java/com/responseor/ootw/dto/WeatherResponseDto.java
+++ src/main/java/com/responseor/ootw/dto/WeatherResponseDto.java
@@ -9,19 +9,13 @@ import lombok.Setter;
 @Getter
 @Builder
 public class WeatherResponseDto {
-    @ApiModelProperty(example = "기상 조건 ID, 기상조건 코드 : https://openweathermap.org/weather-conditions")
+    @ApiModelProperty(example = "기상조건 코드 : https://open-meteo.com/en/docs")
     private int id;
 
-    @ApiModelProperty(example = "날씨 매개 변수 그룹 (비, 눈, 극한 등)")
-    private String main;
-
-    @ApiModelProperty(example = "날씨 아이콘 ID")
-    private String icon;
-
-    @ApiModelProperty(example = "온도. 단위 : 섭씨")
+    @ApiModelProperty(example = "현재 온도. 단위 : 섭씨")
     private float temp;
 
-    @ApiModelProperty(example = "체감 온도. 단위 : 섭씨")
+    @ApiModelProperty(example = "현재 체감 온도. 단위 : 섭씨")
     private float feelsTemp;
 
     @ApiModelProperty(example = "현재 최저 온도(대규모 대도시 및 도시 지역 내)")
