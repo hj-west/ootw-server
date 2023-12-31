07b3816b9c26c5fa42438ab6c93bfca1c61988dc
h.j_west <guswl9814@gmail.com>
2023-12-31 22:58:32 +0900

    feat: 카카오 로그인 테스트용 컨트롤러 구현

--- src/main/java/com/responseor/ootw/controller/AuthController.java
+++ src/main/java/com/responseor/ootw/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.responseor.ootw.controller;
 
+import com.responseor.ootw.dto.auth.KakaoLoginRequestDto;
+import com.responseor.ootw.dto.auth.KakaoTokenResponseDto;
 import com.responseor.ootw.dto.member.MemberLoginRequestDto;
 import com.responseor.ootw.service.AuthService;
 import lombok.AllArgsConstructor;
@@ -19,5 +21,12 @@ public class AuthController {
         String token = authService.login(loginDto.getEmail(), loginDto.getPassword());
 
         return ResponseEntity.ok().body(token);
+    }
+
+    @PostMapping("/oauth/kakao")
+    public ResponseEntity<?> kakaoLogin(@RequestBody KakaoLoginRequestDto loginRequestDto) {
+        KakaoTokenResponseDto responseDto = authService.getAccessToken(loginRequestDto.getCode());
+
+        return ResponseEntity.ok().body(responseDto);
     }
 }
