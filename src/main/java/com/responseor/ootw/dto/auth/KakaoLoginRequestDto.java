7cad449947a0468d2f83d74e066dd944f74913a9
h.j_west <guswl9814@gmail.com>
2023-12-31 22:57:45 +0900

    feat: 카카오 로그인 및 토큰 발급을 위한 dto 생성

---
+++ src/main/java/com/responseor/ootw/dto/auth/KakaoLoginRequestDto.java
@@ -0,0 +1,10 @@
+package com.responseor.ootw.dto.auth;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class KakaoLoginRequestDto {
+    private Integer code;
+}
---
+++ src/main/java/com/responseor/ootw/dto/auth/KakaoTokenResponseDto.java
@@ -0,0 +1,15 @@
+package com.responseor.ootw.dto.auth;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class KakaoTokenResponseDto {
+    private String access_token;
+    private String token_type;
+    private String refresh_token;
+    private Integer expires_in;
+    private String scope;
+    private Integer refresh_token_expires_in;
+}
